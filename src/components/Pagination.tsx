dc4394cd3bfed54ae2b53b998b3db746c2686796
SidraDarmawan <agil77249@gmail.com>
2025-03-20 20:02:00 +0700

    feat: preview image & pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import ReactPaginate from "react-paginate";
+
+interface PaginationProps {
+  total: number;
+  take: number;
+  onChangePage: ({ selected }: { selected: number }) => void;
+}
+
+const Pagination: FC<PaginationProps> = ({ onChangePage, total, take }) => {
+  return (
+    <ReactPaginate
+      onPageChange={onChangePage}
+      pageCount={Math.ceil(total / take)}
+      nextLabel=">"
+      previousLabel="<"
+      pageRangeDisplayed={8}
+      marginPagesDisplayed={2}
+      containerClassName="flex gap-8 w-fit m-4"
+      pageLinkClassName="px-4 py-2 rounded-lg"
+      activeLinkClassName="bg-slate-100/70 text-black rounded-md"
+    />
+  );
+};
+
+export default Pagination;
